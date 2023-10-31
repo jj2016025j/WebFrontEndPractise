00252015a03af955ab82793c32e2027daab49983
jj2016025j <jj2016025j@gmail.com>
2023-11-01 00:29:05 +0800

    javascript練習

--- test/test.js
+++ test/test.js
@@ -1,10 +1,4 @@
-document.ready(function() {
-    ('#toggle-btn').click(function() {
-      ('#slide-element').slideToggle();
-    });
-  });
-
-  const element = document.querySelector(".element");
+  /*const element = document.querySelector(".element");
 
   element.style.transform = "translate(-100px, -100px)"
   
@@ -12,5 +6,48 @@ document.ready(function() {
   element.style.transition = "transform 1s ease-in-out";
   
   // 移動元素
-  element.style.transform = "translate(100px, 100px)";
+  element.style.transform = "translate(100px, 100px)";*/
   
+  //javascript語法練習
+  // let letTest="同var"
+  // const constTest="不可被改變"
+  // console.log(letTest)
+  // let ClassA = ["anne","amy","tom"]
+  // ClassA.push("amumu")
+  // console.log("會換行",ClassA)
+  //ClassA.length
+
+  // let object = {
+  //   image:"../images/1.jpg",
+  //   date:"2023/10/31",
+  //   comment:["好看","好帥","好美"]
+  // }
+  // let wall=[
+  //   object,
+  //   object,
+  //   object,
+  //   object
+  // ]
+  // console.log(wall)
+  // console.log(wall[2])
+  // console.log(wall[0].comment)
+
+  //switch 跟C#一樣
+
+  //這邊建立物件用funtion?
+
+
+  function creatCard(name){
+    this.name=name
+  }
+  let card1=new createCard("Amy")
+  console.log(card1)
+
+  class Card{
+    constructor(name){
+      this.name=name
+    }
+  }
+
+  const card2=new Card("Bruce")
+  console.log(card2)
